0b152bdff9dbe9aa72f6225222ca3f039debdf6f
Darya1705 <darya.zudilina@yandex.ru>
2022-02-22 22:31:36 +0300

    Commit

---
+++ src/main/java/ru/netology/NotFoundException.java
@@ -0,0 +1,10 @@
+package ru.netology;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException() {
+    }
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
